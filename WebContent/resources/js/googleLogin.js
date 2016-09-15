74850d6b80f326c7b9d328081595c1cf7777de51
Thomas Lansing <thompa.lansing@gmail.com>
2016-09-15 22:42:03 +0200

    Fix bug with session and database when switching users.

--- WebContent/resources/js/googleLogin.js
+++ WebContent/resources/js/googleLogin.js
@@ -1,6 +1,6 @@
-/* googleLogin.js */
-
 'use strict';
+
+$('#hiddenGoogleLoginForm').find('input[type=text]').val('');
 
 function onSignIn(googleUser) {
     if (sessionStorage.getItem('isSignedIn') === 'false' || sessionStorage.getItem('isSignedIn') === null) {
@@ -9,11 +9,11 @@ function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
         var id_token = googleUser.getAuthResponse().id_token;
 
+        console.log('User signed in.');
+
         document.getElementById("hiddenGoogleLoginForm:userName").value = profile.getName();
         document.getElementById("hiddenGoogleLoginForm:email").value = profile.getEmail();
         document.getElementById("hiddenGoogleLoginForm:tokenId").value = id_token;
-        // TODO fix this. Won't work :-(
-        //document.getElementById("hiddenGoogleLoginForm").submit();
         $('.submitButton').click();
     }
 }
