30c4c147faf2b87d847b01c17d7475cd55099d72
Thomas Lansing <thompa.lansing@gmail.com>
2016-09-28 19:05:40 +0200

    Add function for resetting searched posts and do some minor refactoring

--- src/main/java/com/pokemongo/controllers/PostController.java
+++ src/main/java/com/pokemongo/controllers/PostController.java
@@ -19,9 +19,9 @@ import java.util.List;
 @Named(value = "postController")
 @SessionScoped
 public class PostController implements Serializable {
-    
+
     private static final long serialVersionUID = -5930735123556838017L;
-    
+
     private String title;
     private String content;
     private String replyContent;
@@ -31,50 +31,38 @@ public class PostController implements Serializable {
     @EJB
     private PostHandler postHandler;
     private static final Logger logger = LogManager.getLogger(PostController.class);
-    
+
     @PostConstruct
     public void init() {
         posts = postHandler.fetchPostsWithoutParent();
     }
-    
+
     public void savePost() {
-        
         Post post = new Post(title, content);
+
         try {
             postHandler.savePost(post);
         } catch (UserNotLoggedInException e) {
+            // TODO add logger
             displayPostFormMessage(e.getMessage());
         }
-        
-        resetFields();
+
+        resetPostFields();
     }
-    
+
     public String saveReply(long postId) {
-        
         Post reply = new Post(replyContent);
+
         try {
             postHandler.saveReply(reply, postId);
         } catch (UserNotLoggedInException e) {
-            //TODO
+            // TODO add logger
         }
-        
-        resetFields();
+
+        resetPostFields();
         return "/index.xhtml?faces-redirect=true";
     }
-    
-    private void resetFields() {
-        
-        logger.debug("Fetching fresh posts...");
-        posts = postHandler.fetchPostsWithoutParent();
-        fetchPostsByKeyword();
 
-        logger.debug("Resetting input fields.");
-
-        title = "";
-        content = "";
-        replyContent = "";
-    }
-    
     public String fetchPostsByKeyword() {
         logger.debug("Fetching posts by keyword: {}", searchWord);
 
@@ -84,64 +72,83 @@ public class PostController implements Serializable {
         } catch (FormException e) {
             logger.error(e);
             displayPostFormMessage("Please type more than two characters");
-            // TODO fix this. Page refreshes anyway!
             return "/index.xhtml?faces-redirect=false";
         }
     }
-    
+
+    public void resetSearchedPosts() {
+        setPostSearchResults(null);
+        searchWord = "";
+    }
+
+    private void resetPostFields() {
+        fetchFreshPosts();
+
+        logger.debug("Resetting input fields.");
+        title = "";
+        content = "";
+        replyContent = "";
+    }
+
+    private void fetchFreshPosts() {
+        logger.debug("Fetching fresh posts...");
+        posts = postHandler.fetchPostsWithoutParent();
+        fetchPostsByKeyword();
+    }
+
     private void displayPostFormMessage(String message) {
         FacesMessage facesMessage = new FacesMessage(message);
         FacesContext.getCurrentInstance().addMessage("formId:postForm", facesMessage);
     }
 
     /* Getters and Setters */
-    
+
     public String getTitle() {
         return title;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public String getContent() {
         return content;
     }
-    
+
     public void setContent(String content) {
         this.content = content;
     }
-    
+
     public String getReplyContent() {
         return replyContent;
     }
-    
+
     public void setReplyContent(String replyContent) {
         this.replyContent = replyContent;
     }
-    
+
     public String getSearchWord() {
         return searchWord;
     }
-    
+
     public void setSearchWord(String searchWord) {
         this.searchWord = searchWord;
     }
-    
+
     public List<Post> getPostSearchResults() {
         return postSearchResults;
     }
-    
+
     public void setPostSearchResults(List<Post> postSearchResults) {
         this.postSearchResults = postSearchResults;
     }
-    
+
     public List<Post> getPosts() {
         return posts;
     }
-    
+
     public void setPosts(List<Post> posts) {
         this.posts = posts;
     }
-    
+
 }
