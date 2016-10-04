4ad511ee8fe56ed91ed301ef0ce0709946708e77
LucianaHaugen <lucianahaugen@gmail.com>
2016-10-04 12:29:01 +0200

    Userid saved with pokemon

--- src/main/java/com/pokemongo/business/PokemonEJB.java
+++ src/main/java/com/pokemongo/business/PokemonEJB.java
@@ -1,23 +1,33 @@
 package com.pokemongo.business;
 
+import com.pokemongo.models.User;
 import com.pokemongo.services.PokemonService;
 import com.pokemongo.business.interfaces.PokemonHandler;
 import com.pokemongo.models.Pokemon;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import java.util.Map;
 
 @Stateless
 public class PokemonEJB implements PokemonHandler {
-    @EJB
-    private UserEJB userEjb;
+
     @EJB
     private PokemonService pokemonService;
+    private ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+    private Map<String, Object> sessionMap = externalContext.getSessionMap();
+    private static final Logger logger = LogManager.getLogger(PokemonEJB.class);
 
     @Override
     public void savePokemon(Pokemon pokemon) {
-        
+        User owner = (User)sessionMap.get("loggedInUser");
+        logger.debug(owner);
+        pokemon.setOwner(owner);
         pokemonService.savePokemon(pokemon);
     }
 
