2ba605849c3cd76348825d1352381b1a3fe85bb5
Thomas Lansing <thompa.lansing@gmail.com>
2016-10-06 16:34:30 +0200

    Rework the pokemon rest provider to return status codes instead

--- src/main/java/com/pokemongo/rest/PokemonRestProvider.java
+++ src/main/java/com/pokemongo/rest/PokemonRestProvider.java
@@ -8,24 +8,32 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import java.util.List;
+import javax.ws.rs.core.Response;
 
 @Path("/pokemon")
 public class PokemonRestProvider {
-    
+
     @EJB
     private PokemonDataService pokemonDataService;
-    
+
     @GET
     @Produces("application/json")
-    public List<PokemonData> getAllPokemon() {
-        return pokemonDataService.fetchAllPokemonData();
+    public Response getAllPokemon() {
+        return Response.status(Response.Status.OK)
+                .entity(pokemonDataService.fetchAllPokemonData())
+                .build();
     }
-    
+
     @GET
-    @Path("/{id}")
+    @Path("/{pokedexNr}")
     @Produces("application/json")
-    public PokemonData getPokemon(@PathParam("id") int pokedexNr) {
-        return pokemonDataService.fetchPokemonDataByPokedexNumber(pokedexNr);
+    public Response getPokemon(@PathParam("pokedexNr") int pokedexNr) {
+        PokemonData pokemon = pokemonDataService.fetchPokemonDataByPokedexNumber(pokedexNr);
+
+        if (pokemon == null)
+            return Response.status(Response.Status.NO_CONTENT).build();
+
+        return Response.status(Response.Status.OK).entity(pokemon).build();
     }
+
 }
--- src/main/java/com/pokemongo/services/PokemonDataService.java
+++ src/main/java/com/pokemongo/services/PokemonDataService.java
@@ -1,9 +1,12 @@
 package com.pokemongo.services;
 
 import com.pokemongo.models.PokemonData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
@@ -13,10 +16,17 @@ public class PokemonDataService {
     @PersistenceContext
     private EntityManager em;
 
+    private static final Logger logger = LogManager.getLogger(PokemonDataService.class);
+
     public PokemonData fetchPokemonDataByPokedexNumber(int pokedexNumber) {
-        return em.createNamedQuery("PokemonData.fetchPokemonDataByPokedexNumber", PokemonData.class)
-                .setParameter("pokedexNumber", pokedexNumber)
-                .getSingleResult();
+        try {
+            return em.createNamedQuery("PokemonData.fetchPokemonDataByPokedexNumber", PokemonData.class)
+                    .setParameter("pokedexNumber", pokedexNumber)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            logger.error("No pokemon found with that pokedex number");
+            return null;
+        }
     }
 
     public PokemonData fetchPokemonData(long id) {
@@ -26,4 +36,5 @@ public class PokemonDataService {
     public List<PokemonData> fetchAllPokemonData() {
         return em.createNamedQuery("PokemonData.fetchAll").getResultList();
     }
+
 }
