6e83297bb49155a5b25d3ca8369ad40923c2635b
Tobias <tobias.ljungstrom@gmail.com>
2016-10-10 16:03:02 +0200

    Check that there is a file to upload before uploading. Add a nicer gui for upload

---
+++ WebContent/resources/js/user.js
@@ -0,0 +1,9 @@
+/* user.js */
+
+'use strict';
+
+$(document).ready(function() {
+    $('.userInfo__uploadShower').click(function() {
+        $('.userInfo__upload').slideToggle(200);
+    });
+});
--- src/main/java/com/pokemongo/controllers/FileController.java
+++ src/main/java/com/pokemongo/controllers/FileController.java
@@ -31,30 +31,33 @@ public class FileController implements Serializable{
     private Part upload;
     
     public void uploadImage() {
+        
+        if (upload != null) {
     
-        User currentUser = userHandler.getLoggedInUser();
+            User currentUser = userHandler.getLoggedInUser();
     
-        try {
-            //Make the uploaded file into a stream
-            InputStream inputStream = upload.getInputStream();
-            
-            // Get info to build the file name
-            String id = currentUser.getId().toString();
-            String extension = determineExtension(upload);
-
-            // Create a file with the correct, calculated path
-            File file = new File(System.getProperty("jboss.server.data.dir") + "/images");
-            File finalFile = new File(file, id + extension);
-
-            // Write the stream to disk
-            Files.copy(inputStream, finalFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            
-            //Save the filename with the user for later use
-            currentUser.setUserImageName(id + extension);
-            userHandler.saveUser(currentUser);
-
-        } catch (IOException | DatabaseException e) {
-            logger.error(e.getMessage());
+            try {
+                //Make the uploaded file into a stream
+                InputStream inputStream = upload.getInputStream();
+        
+                // Get info to build the file name
+                String id = currentUser.getId().toString();
+                String extension = determineExtension(upload);
+        
+                // Create a file with the correct, calculated path
+                File file = new File(System.getProperty("jboss.server.data.dir") + "/images");
+                File finalFile = new File(file, id + extension);
+        
+                // Write the stream to disk
+                Files.copy(inputStream, finalFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        
+                //Save the filename with the user for later use
+                currentUser.setUserImageName(id + extension);
+                userHandler.saveUser(currentUser);
+        
+            } catch (IOException | DatabaseException e) {
+                logger.error(e.getMessage());
+            }
         }
     }
     
