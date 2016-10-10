99c93881828cfd8914c8d576e90a91e20439b811
Thomas Lansing <thompa.lansing@gmail.com>
2016-10-10 13:58:12 +0200

    Fix various issues and work on smoother user experiance while adding pokemon

--- WebContent/resources/js/map.js
+++ WebContent/resources/js/map.js
@@ -11,7 +11,6 @@ function initMap() {
     });
 
     // This event listener will call addPokemonLatLong() when the map is clicked.
-
     map.addListener('click', function (event) {
         var pok_id = document.getElementById('savingPokemon:pokemonSelectMenuInner').value;
         addPokemonLatLong(parseFloat(event.latLng.lat()),
@@ -21,24 +20,64 @@ function initMap() {
 
 // Adds a marker to the map and push to the array.
 function addPokemonLatLong(lati, longi, pokeIndex) {
-    document.getElementById('input_savingPokemon:lng').value = longi;
-    document.getElementById('input_savingPokemon:lat').value = lati;
-    var imageLink = link(pokeIndex);
+    // Delete previous marker while adding pokemon
+    deleteMarkers();
+
+    // Set the coords in the hidden input fields
+    setCoords(longi, lati);
+
+    // Fetch and set the image for the marker
+    var imageLink = setImageLink(pokeIndex);
+
+    // Create the marker
     var marker = new google.maps.Marker({
         position: {
             lat: parseFloat(lati),
             lng: parseFloat(longi)
         },
         map: map,
-        // randomize pokemon
         icon: imageLink,
         draggable: true,
         title: "Pokemon!"
     });
+
+    // Push the marker to markers array
     markers.push(marker);
+
+    // Attach a listener to the marker which updates coords when after dragging
+    google.maps.event.addListener(marker, 'dragend', function (event) {
+        setCoords(event.latLng.lng(), event.latLng.lat());
+    })
 }
 
+function setCoords(longi, lati) {
+    document.getElementById('input_savingPokemon:lng').value = longi;
+    document.getElementById('input_savingPokemon:lat').value = lati;
+}
 
-function link(index) {
+function setImageLink(index) {
     return "http://www.serebii.net/pokearth/sprites/em/" + index + ".png";
+}
+
+// Sets the map on all markers in the array
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
 }
