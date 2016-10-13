2edb17aa243a81f263e45755ec595b1a3315a0ef
Thomas Lansing <thompa.lansing@gmail.com>
2016-10-13 09:38:33 +0200

    Fix failing tests for PostEJB

--- src/test/com/pokemongo/business/PostEJBTest.java
+++ src/test/com/pokemongo/business/PostEJBTest.java
@@ -1,5 +1,6 @@
 package com.pokemongo.business;
 
+import com.pokemongo.business.interfaces.UserHandler;
 import com.pokemongo.exceptions.DatabaseException;
 import com.pokemongo.exceptions.FormException;
 import com.pokemongo.exceptions.UserException;
@@ -9,26 +10,19 @@ import com.pokemongo.services.PostService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({FacesContext.class})
+// @RunWith(PowerMockRunner.class)
+// @PrepareForTest({FacesContext.class})
 public class PostEJBTest {
 
     // Mock the PostService
@@ -36,12 +30,14 @@ public class PostEJBTest {
     private PostService mockedPostService;
 
     // Mocks for the context
+    // @Mock
+    // private Map<String, Object> mockedSessionMap;
+    // @Mock
+    // private ExternalContext mockedExternalContext;
+    // @Mock
+    // private FacesContext mockedFacesContext;
     @Mock
-    private Map<String, Object> mockedSessionMap;
-    @Mock
-    private ExternalContext mockedExternalContext;
-    @Mock
-    private FacesContext mockedFacesContext;
+    private UserHandler userHandler;
 
     // Inject above mocks in the class to be tested
     @InjectMocks
@@ -59,11 +55,13 @@ public class PostEJBTest {
 
         // Initialize the fake objects
         testLoggedInUser = new User("User Name", "User Email", "User Token");
+
         testPost = new Post("Post Title", "Post Content");
-        testReply = new Post(null, "Reply Content");
         testPostList = new ArrayList<>();
         testPostList.add(testPost);
         testPostList.add(testReply);
+
+        testReply = new Post(null, "Reply Content");
 
         // Initialize the mocks
         MockitoAnnotations.initMocks(this);
@@ -79,13 +77,16 @@ public class PostEJBTest {
         // TODO find out a way to mock out the logger. All the test runs - but with trivial errors
 
         // Mock the context
-        PowerMockito.mockStatic(FacesContext.class);
-        when(FacesContext.getCurrentInstance()).thenReturn(mockedFacesContext);
-        when(mockedFacesContext.getExternalContext()).thenReturn(mockedExternalContext);
-        when(mockedExternalContext.getSessionMap()).thenReturn(mockedSessionMap);
+        // PowerMockito.mockStatic(FacesContext.class);
+        // when(FacesContext.getCurrentInstance()).thenReturn(mockedFacesContext);
+        // when(mockedFacesContext.getExternalContext()).thenReturn(mockedExternalContext);
+        // when(mockedExternalContext.getSessionMap()).thenReturn(mockedSessionMap);
 
         // Mock the logged in user
-        when(mockedSessionMap.get("loggedInUser")).thenReturn(testLoggedInUser);
+        // when(mockedSessionMap.get("loggedInUser")).thenReturn(testLoggedInUser);
+
+        // Mock the methods in UserHandler
+        when(userHandler.getLoggedInUser()).thenReturn(testLoggedInUser);
     }
 
     @After
@@ -105,7 +106,7 @@ public class PostEJBTest {
 
     @Test(expected = UserException.class)
     public void testSavePost_UserNotLoggedIn_UserNotLoggedInExceptionThrown() throws UserException, DatabaseException {
-        when(mockedSessionMap.get("loggedInUser")).thenReturn(null);
+        when(userHandler.getLoggedInUser()).thenReturn(null);
         postEJB.savePost(testPost);
     }
 
@@ -117,7 +118,7 @@ public class PostEJBTest {
 
     @Test(expected = UserException.class)
     public void testSaveReply_UserNotLoggedIn_UserNotLoggedInExceptionThrown() throws UserException, DatabaseException {
-        when(mockedSessionMap.get("loggedInUser")).thenReturn(null);
+        when(userHandler.getLoggedInUser()).thenReturn(null);
         postEJB.saveReply(testPost, 1L);
     }
 
