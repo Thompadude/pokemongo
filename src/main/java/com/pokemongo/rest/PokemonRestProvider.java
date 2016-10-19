dd34d0469f2cfb36c662f072ae915dc2e3abce30
Tobias <tobias.ljungstrom@gmail.com>
2016-10-19 11:44:49 +0200

    Add HATEOAS links (and restful endpoint) for a single owned pokemon

--- src/main/java/com/pokemongo/rest/PokemonRestProvider.java
+++ src/main/java/com/pokemongo/rest/PokemonRestProvider.java
@@ -2,12 +2,16 @@ package com.pokemongo.rest;
 
 import com.pokemongo.models.Pokemon;
 import com.pokemongo.services.PokemonService;
+import com.pokemongo.utilities.RestLinkBuilder;
 
 import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 @Path("/pokemon")
@@ -15,10 +19,11 @@ public class PokemonRestProvider {
 
     @EJB
     private PokemonService pokemonService;
-
+    private RestLinkBuilder<PokemonRestProvider> pokemonRestLinkBuilder = new RestLinkBuilder<>(PokemonRestProvider.class);
+    
     @GET
     @Produces("application/json")
-    public Response getPokemonService() {
+    public Response getAllPokemon() {
         List<Pokemon> pokemonList = pokemonService.fetchAllPokemons();
 
         if (pokemonList.isEmpty()) {
@@ -26,6 +31,21 @@ public class PokemonRestProvider {
         }
 
         return Response.status(Response.Status.OK).entity(pokemonList).build();
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Response getPokemonById(@PathParam("id") Long id, @Context UriInfo uriInfo) {
+        Pokemon pokemon = pokemonService.fetchPokemonById(id);
+        
+        if (pokemon == null)
+            return Response.status(Response.Status.NO_CONTENT).build();
+    
+        pokemon.addRestLink(pokemonRestLinkBuilder.getSelfLink(pokemon.getId(), uriInfo));
+        pokemon.addRestLink(pokemonRestLinkBuilder.getUserLink(pokemon, uriInfo, "Owner"));
+        
+        return Response.status(Response.Status.OK).entity(pokemon).build();
     }
 
 }
