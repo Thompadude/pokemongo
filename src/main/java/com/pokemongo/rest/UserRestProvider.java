4418eab5edd0f10bab8126ce03e74df54a9743ac
Thomas Lansing <thompa.lansing@gmail.com>
2016-11-03 16:21:38 +0100

    Refactor REST service for users

--- src/main/java/com/pokemongo/rest/UserRestProvider.java
+++ src/main/java/com/pokemongo/rest/UserRestProvider.java
@@ -28,16 +28,15 @@ public class UserRestProvider {
     public Response getAllUserPokemon(@PathParam("id") int id, @Context UriInfo uriInfo) {
         User user = userService.fetchUser(id);
 
-        if (user == null || user.getPokemons().isEmpty())
-            return Response.status(Response.Status.NO_CONTENT).build();
-
-        setPokemonLinks(uriInfo, user);
-        for (Pokemon pokemon :
-                user.getPokemons()) {
-            pokemon.addRestLink(userRestLinkBuilder.getUserLink(pokemon, uriInfo, "Owner"));
+        if (!(user == null) && !user.getPokemons().isEmpty()) {
+            setPokemonLinks(uriInfo, user);
+            for (Pokemon pokemon : user.getPokemons()) {
+                pokemon.addRestLink(userRestLinkBuilder.getUserLink(pokemon, uriInfo, "Owner"));
+            }
+            return Response.status(Response.Status.OK).entity(user.getPokemons()).build();
         }
 
-        return Response.status(Response.Status.OK).entity(user.getPokemons()).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @GET
@@ -46,15 +45,15 @@ public class UserRestProvider {
     public Response getUser(@PathParam("id") int id, @Context UriInfo uriInfo) {
         User user = userService.fetchUser(id);
 
-        if (user == null)
-            return Response.status(Response.Status.NO_CONTENT).build();
+        if (!(user == null)) {
+            user.addRestLink(userRestLinkBuilder.getSelfLink(user.getId(), uriInfo));
+            user.addRestLink(userRestLinkBuilder.getPokemonLink(user.getId(), uriInfo));
+            setPokemonLinks(uriInfo, user);
 
-        user.addRestLink(userRestLinkBuilder.getSelfLink(user.getId(), uriInfo));
-        user.addRestLink(userRestLinkBuilder.getPokemonLink(user.getId(), uriInfo));
+            return Response.status(Response.Status.OK).entity(user).build();
+        }
 
-        setPokemonLinks(uriInfo, user);
-
-        return Response.status(Response.Status.OK).entity(user).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     private void setPokemonLinks(@Context UriInfo uriInfo, User user) {
